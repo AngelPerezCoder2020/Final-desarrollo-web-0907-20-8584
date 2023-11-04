1314b1f8f4230ae9ebe52ae2d764e9c3be555f7b
AngelPerezCoder2020 <angelperez.cmp.b.16@gmail.com>
2023-11-04 10:40:14 -0600

    se agregaron librerias

--- config/sql_server.js
+++ config/sql_server.js
@@ -1,12 +1,24 @@
-
-const config ={
-    server: process.env.SQLSERVER_SERVIDOR,
-    user: process.env.SQLSERVER_USER,
+//const config ={
+  //  server: process.env.SQLSERVER_SERVIDOR,
+    //user: process.env.SQLSERVER_USER,
+    //password: process.env.SQLSERVER_ROOT_PASSWORD,
+    //database: process.env.SQLSERVER_DATABASE,
+    //options: {
+    //    trustServerCertificate: true
+    //}
+//}
+const config = {
+    user: process.env.SQLSERVER_SERVIDOR,
     password: process.env.SQLSERVER_ROOT_PASSWORD,
+    server: process.env.SQLSERVER_SERVIDOR,
     database: process.env.SQLSERVER_DATABASE,
+    pool: {
+        max: 10,
+        min: 0,
+        idleTimeoutMillis: 30000
+    },
     options: {
         trustServerCertificate: true
     }
 }
-
 module.exports.config = config;
